667caf02bfef20de1ea2812f55ddb455feb8946f
Emersoaresj <emersonsoares269@icloud.com>
2025-07-18 18:47:24 -0300

    primeiro commit

---
+++ src/main/java/com/fiap/postech/pedido_receiver/PedidoReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.pedido_receiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoReceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/postech/pedido_receiver/PedidoReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.pedido_receiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
